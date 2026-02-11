ff9462abe0c64eb5695d64ee8309587e6f8abb9d
TosDaBoss <tosdaboss@users.noreply.github.com>
2026-02-11 22:54:34 +0000

    Add Google Analytics tag

--- landing/app/layout.tsx
+++ landing/app/layout.tsx
@@ -24,6 +24,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17948130851"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17948130851');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
